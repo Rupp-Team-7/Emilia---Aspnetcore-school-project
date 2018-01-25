6536a3edac64d382b14fa8c84e89371647c03af4
angelplayer <angel.player@outlook.com>
2018-01-25 23:59:02 +0700

    add form for create new product

--- Emilia/Controllers/ProductController.cs
+++ Emilia/Controllers/ProductController.cs
@@ -1,19 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Emilia.Data;
+using Emilia.Models;
+using Emilia.Models.ProductViewModel;
 
 namespace Emilia.Controllers
 {
     public class ProductController : Controller
     {
         private ApplicationDbContext db;
+        private UserManager<ApplicationUser> manager;
 
-        public ProductController(ApplicationDbContext db )
+        public ProductController(ApplicationDbContext db, UserManager<ApplicationUser> manager)
         {
             this.db = db;
+            this.manager = manager;
         }
 
         // GET: /Product/index
@@ -26,6 +35,44 @@ namespace Emilia.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        //POST: /Product/Create
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductViewModel model)
+        {
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = await manager.GetUserAsync(HttpContext.User);
+            var seller = await db.Sellers.Where(s => s.Id == user.SellerID).AsNoTracking().SingleOrDefaultAsync();
+
+            var product = new Product{
+                Name = model.Name,
+                category = model.Category,
+                UnitPrice = model.UnitPrice,
+                SellerId = seller.Id,
+                Details = new ProductDetail
+                {
+                    BrandName = string.IsNullOrEmpty( model.BrandName) ? "No Brand" : model.BrandName,
+                    Description = model.Description,
+                    Specification = model.Specification,
+                    Origin = string.IsNullOrEmpty(model.Origin) ? "N/A" : model.Origin,
+                    Material = model.ProductCode
+                }
+            };
+
+            await this.db.Products.AddAsync(product);
+            await this.db.SaveChangesAsync();
+
+            var created = await db.Products.AsNoTracking().Where(p => p.Id == product.Id).Include(p => p.Details).SingleOrDefaultAsync();
+
+            return Ok(created);
+
         }
 
         // GET: /Product/Edit/1
---
+++ Emilia/Models/ProductViewModel/CreateProductViewModel.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace Emilia.Models.ProductViewModel
+{
+
+    public class CreateProductViewModel
+    {
+        [MaxLength(12)]
+        [Display(Name = "Product Code")]
+        public string ProductCode { get; set; } 
+        
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Required][Display(Name = "Unit prices")]
+        public decimal UnitPrice {get; set;}
+
+        [MaxLength(30)][Display(Name = "Tags")]
+        public string Tags {get; set;}
+
+        [Display(Name = "Brand name")]
+        public string BrandName {get; set;}
+        public string Origin { get; set; }
+
+        [MaxLength(150)]
+        public string Description {get; set;}
+
+        [MaxLength(150)]
+        public string Specification {get; set;}
+
+        public IFormFile Files {get; set;}
+
+    }
+
+
+}

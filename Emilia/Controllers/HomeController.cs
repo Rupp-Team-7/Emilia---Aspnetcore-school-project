c8abb1d3c7e522d8956df632911ad3fbca356da5
angelplayer <angel.player@outlook.com>
2018-02-17 02:11:14 +0700

    add data to index page

--- Emilia/Controllers/HomeController.cs
+++ Emilia/Controllers/HomeController.cs
@@ -7,17 +7,42 @@ using Microsoft.AspNetCore.Mvc;
 using Emilia.Models;
 using Emilia.Data;
 using Microsoft.EntityFrameworkCore;
+using Emilia.Extensions;
+using Emilia.Models.HomeViewmodel;
 
 namespace Emilia.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db;
 
+        public HomeController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
        
         //Get: /, /home/
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = db.Products.AsNoTracking().Where(x => x.Published).OrderBy(x => x.Created);
+            var seller = db.Sellers.AsNoTracking().OrderBy(x => x.CreatedDate);
+
+            var model = new IndexViewmodel {
+                ShopItems = await products.Select(p => new ShopItem {
+                    Name = p.Name,
+                    Price = p.UnitPrice,
+                    Image= p.FirstImage(),
+                    Id = p.Id
+                }).Take(16).ToListAsync(),
+
+                SellerItems = await seller.Select(s => new SellerItem {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Logo = s.Logo
+                }).Take(8).ToListAsync()
+            };
+
+            return View(model);
         }
 
         //Get: /Search?querystring...

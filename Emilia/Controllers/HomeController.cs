630fc3523c3cb42ae50f0e81ab5dd0a91bccc9d0
ASUS <ASUS@RITH-PC>
2018-03-01 14:49:37 +0700

    f

--- Emilia/Controllers/HomeController.cs
+++ Emilia/Controllers/HomeController.cs
@@ -135,7 +135,7 @@ namespace Emilia.Controllers
             //var cus = await db.Customers.Where(s => s.Id == user.CustomerID).AsNoTracking().SingleOrDefaultAsync();
             var order = new Order
             {
-                CustomerId = user.CustomerID,
+                CustomerId = cid,
                 Quanity = modal.Quanity,
                 SellerId = modal.SellerId,
                 TotalPrice = modal.TotalPrice,
